ba3876ea3d09a6c1e89e0fbe1f0e9a2cd976a391
vpcsilva <vpcx5@yahoo.com.br>
2015-07-22 11:57:47 -0300

    adicionando o codigo

---
+++ 2015.1/lab_grafos/codigo/Kruskal/Kruskal/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Kruskal
+//
+//  Created by Victor Silva on 7/22/15.
+//  Copyright (c) 2015 AA Grafos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
